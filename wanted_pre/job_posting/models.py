16cc4a5b92b352645da255cffb461c02622e4db0
khw7876 <khw7876@gmail.com>
2022-08-22 20:49:25 +0900

    feat/ 초기 기본모델 생성

--- wanted_pre/job_posting/models.py
+++ wanted_pre/job_posting/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 # Create your models here.
+ #회사, 사용자, 채용공고, 지원내역(선택사항)
+class Company(models.Model):
+    name = models.CharField("회사이름", max_length=20)
+    country = models.CharField("국가", max_length=20)
+    region = models.CharField("지역", max_length=20)
 
 
+class JobPosting(models.Model):
+    company = models.ForeignKey("Company", on_delete=models.CASCADE)
+    job_position = models.CharField("채용포지션", max_length=20)
+    compensation = models.IntegerField("채용 보상금", default=0)
+    skill = models.CharField("사용 기술", max_length=20)
+    content = models.TextField("채용내용", max_length=200)
+
+
+class User(models.Model):
+    user = models.CharField("사용자", max_length=20)
+    
 
     
