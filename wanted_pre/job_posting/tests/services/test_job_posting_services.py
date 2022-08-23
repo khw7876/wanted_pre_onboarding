46934999e3c7aa8f5be26ac9a0499adeb148857e
khw7876 <khw7876@gmail.com>
2022-08-23 23:16:19 +0900

    test/ 2번문제에 대한 test코드 작성 및 에러 핸들링

--- wanted_pre/job_posting/tests/services/test_job_posting_services.py
+++ wanted_pre/job_posting/tests/services/test_job_posting_services.py
@@ -7,8 +7,10 @@ from job_posting.models import CompanyRegion as CompanyRegionModel
 from job_posting.models import Skill as SkillModel
 from job_posting.models import JobPosition as JobPositionModel, JobPosting as JobPostingModel
 from job_posting.services.job_posting_service import create_job_post
+from job_posting.services.job_posting_service import update_job_post
 
-DOES_NOT_EXIST_NUM = -1
+
+DOES_NOT_EXIST_NUM = 0
 
 class TestCreateJobPost(TestCase):
     """
@@ -19,9 +21,10 @@ class TestCreateJobPost(TestCase):
     def setUpTestData(cls):
         county_for_test = CompanyCountryModel.objects.create(country = "한국")
         region_for_test = CompanyRegionModel.objects.create(region = "서울")
-        SkillModel.objects.create(skill_name="python")
-        JobPositionModel.objects.create(job_position="개발자직군")
-        CompanyModel.objects.create(name="1번회사", country=county_for_test, region=region_for_test)
+        skill_for_test = SkillModel.objects.create(skill_name="python")
+        job_postition_for_test = JobPositionModel.objects.create(job_position="개발자직군")
+        company_for_test = CompanyModel.objects.create(name="1번회사", country=county_for_test, region=region_for_test)
+        JobPostingModel.objects.create(company=company_for_test, job_position=job_postition_for_test, compensation=1000000, content="test채용공고", skill=skill_for_test)
 
     def test_create_job_post(self):
         """
@@ -33,11 +36,10 @@ class TestCreateJobPost(TestCase):
         skill_id = SkillModel.objects.get(skill_name = "python").id
 
         test_request_data = {"company" : company_id, "job_position" : job_position_id, "compensation" : 100000, "content" : "회사소개", "skill" :skill_id}
-        create_job_post(test_request_data)
+        
 
-        self.assertEqual(
-            JobPostingModel.objects.all().count(), 1
-        )
+        with self.assertNumQueries(4):
+            create_job_post(test_request_data)
 
     def test_when_create_job_post_does_not_exist_company_data(self):
         """
@@ -91,3 +93,49 @@ class TestCreateJobPost(TestCase):
 
         with self.assertRaises(exceptions.ValidationError):
             create_job_post(test_request_data)
+
+    def test_update_job_post(self):
+        """
+        채용공고를 수정하는 service함수 검증
+        case : 정상적으로 작동을 했을 경우
+        """
+        job_post_id_for_updata = JobPostingModel.objects.get(content="test채용공고").id
+        test_request_data = {"compensation" : "99999"}
+
+        with self.assertNumQueries(2):
+            update_job_post(job_post_id_for_updata, test_request_data)
+    
+    def test_when_update_job_post_does_not_exist_company_data(self):
+        """
+        채용공고를 수정하는 service함수 검증
+        case: 없는 회사 데이터를 입력했을 경우
+        """
+        job_post_id_for_updata = JobPostingModel.objects.get(content="test채용공고").id
+
+        test_request_data = {"company" : DOES_NOT_EXIST_NUM}
+        
+        with self.assertRaises(exceptions.ValidationError):
+            update_job_post(job_post_id_for_updata, test_request_data)
+
+    def test_when_update_job_post_content_is_over_200_length(self):
+        """
+        채용공고를 수정하는 service함수 검증
+        case : content 제한수 200을 초과하였을 경우
+        """
+        job_post_id_for_updata = JobPostingModel.objects.get(content="test채용공고").id
+
+        test_request_data = {"content" : str("A"*210)}
+        
+        with self.assertRaises(exceptions.ValidationError):
+            update_job_post(job_post_id_for_updata, test_request_data)
+
+    def test_when_update_job_post_content_has_worng_post_id(self):
+        """
+        채용공고를 수정하는 service함수 검증
+        case : 존재하지 않는 jobpost_id를 받았을 경우
+        """
+
+        test_request_data = {"content" : "content수정중"}
+        
+        with self.assertRaises(JobPostingModel.DoesNotExist):
+            update_job_post(DOES_NOT_EXIST_NUM, test_request_data)
--- wanted_pre/job_posting/views.py
+++ wanted_pre/job_posting/views.py
@@ -6,6 +6,7 @@ from rest_framework import status, exceptions
 
 from job_posting.services.job_posting_service import create_job_post
 from job_posting.services.job_posting_service import update_job_post
+from job_posting.models import JobPosting as JobPostingModel
 # Create your views here.
 
 class JobPostView(APIView):
@@ -21,5 +22,10 @@ class JobPostView(APIView):
             return Response({"detail": "모델에 존재하지 않는 PK를 받고 있습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, job_post_id):
-        update_job_post(job_post_id, request.data)
-        return Response({"detail" : "채용공고가 수정 되었습니다."}, status=status.HTTP_200_OK)
+        try:
+            update_job_post(job_post_id, request.data)
+            return Response({"detail" : "채용공고가 수정 되었습니다."}, status=status.HTTP_200_OK)
+        except exceptions.ValidationError:
+            return Response({"detail": "모델에 존재하지 않는 PK를 받고 있습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        except JobPostingModel.DoesNotExist:
+            return Response({"detail": "존재하지 않는 jobPost입니다."}, status=status.HTTP_404_NOT_FOUND)
