6ae8df0cfd26fd015771dc89fd8ab8910ca0db42
khw7876 <khw7876@gmail.com>
2022-08-23 17:59:43 +0900

    feat/ 2번문제 수정기능 구현

--- wanted_pre/job_posting/services/job_posting_service.py
+++ wanted_pre/job_posting/services/job_posting_service.py
@@ -1,7 +1,9 @@
+from functools import partial
+from typing import Dict
 from job_posting.models import JobPosting as JobPostingModel
 from job_posting.serializers import JobPostingSerializer
 
-def create_job_post(jobpost_data):
+def create_job_post(jobpost_data : Dict[str, str]) -> None:
     """
     1. 새로운 채용공고를 등록하는 service
     """
@@ -9,3 +11,11 @@ def create_job_post(jobpost_data):
     create_jobpost_serializer.is_valid(raise_exception=True)
     create_jobpost_serializer.save()
 
+def update_job_post(job_post_id : int, jobpost_data) -> None:
+    """
+    2. 기존의 채용공고를 수정하는 service
+    """
+    jobpost_for_update = JobPostingModel.objects.get(id=job_post_id)
+    update_jobpost_serializer = JobPostingSerializer(jobpost_for_update, data=jobpost_data, partial=True)
+    update_jobpost_serializer.is_valid(raise_exception=True)
+    update_jobpost_serializer.save()
