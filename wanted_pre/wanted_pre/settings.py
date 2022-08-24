3a1391e98103c146da53116a233506e4c99b265d
khw7876 <khw7876@gmail.com>
2022-08-24 17:54:59 +0900

    refac/ SECREAT_KEY 재설정 및 숨기기

--- wanted_pre/wanted_pre/settings.py
+++ wanted_pre/wanted_pre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=iu_#li^4p#&23(&05fp%&)w=t$4^xyhz9sziwvpyij#_(s!j!'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'hyun')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
